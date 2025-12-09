48ed50aed688edd40999d07c3b7b4e7af76cd2d2
dduzgun-security <deno@mac.home>
2025-12-08 23:49:23 -0500

    fix action

--- jug-consent-service-js/index.js
+++ jug-consent-service-js/index.js
@@ -3,3 +3,25 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { ConsentRequest, ConsentResponse } = require('@dduzgun-security/jug-model');
 
+const PORT = process.env.PORT || 8000;
+
+express()
+    .use(function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        next();
+    })
+    .use(morgan('tiny'))
+    .use(bodyParser.json())
+    .post('/consent', (req, res) => {
+        try {
+            const consentRequest = ConsentRequest.deserializeBinary(Buffer.from(req.body.data, 'base64'));
+            const consent = consentRequest.getConsent();
+            
+            console.log(`Received consent from user ${consent.getUserId()}: ${consent.getConsent()}`);
+        } catch (error) {
+            console.error('Error processing consent request:', error);
+            res.status(400).send('Invalid consent request');
+        }
+    })
--- jug-model/jug-model-js/index.js
+++ jug-model/jug-model-js/index.js
@@ -1,5 +1,6 @@
 // Export all generated protobuf models
 module.exports = {
   poutine: require('./rating/poutine/v1/poutine_pb.js'),
-  user: require('./rating/user/v1/user_pb.js')
+  user: require('./rating/user/v1/user_pb.js'),
+  consent: require('./rating/consent/v1/consent_pb.js')
 };
